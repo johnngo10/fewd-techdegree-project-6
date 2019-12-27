3e4bfc2351998a7224b15bbba386bd9b63688871
xuanngo10 <xuan.ngo10@gmail.com>
2019-12-27 12:49:27 -0800

    completed basic functionalities

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
-const missed = 0;
 const startButton = document.querySelector(".btn__reset");
+const scoreboard = document.querySelector("#scoreboard");
+var missed = 0;
 
 const phrases = [
   "javascript is fun",
@@ -43,7 +44,7 @@ const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
 // Check players buttons against correct letters
-const checkLetter = btnGuess => {
+function checkLetter(btnGuess) {
   const letter = document.querySelectorAll(".letter");
   let match = null;
   for (let i = 0; i < letter.length; i++) {
@@ -53,14 +54,37 @@ const checkLetter = btnGuess => {
     }
   }
   return match;
-};
+}
 
+// Listen for players button press and score
 qwerty.addEventListener("click", btn => {
   const button = btn.target;
   if (button.tagName === "BUTTON" && button.className !== "chosen") {
     button.className = "chosen";
-    const letterFound = checkLetter(button);
   } else if (button.tagName === "BUTTON" || button.className === "chosen") {
     button.setAttribute("disabled", "");
   }
+  const letterFound = checkLetter(button);
+  if (letterFound === null) {
+    const tries = scoreboard.firstElementChild;
+    tries.removeChild(tries.firstElementChild);
+    missed += 1;
+  }
+  checkWin(button);
 });
+
+// Check for win or loss
+function checkWin() {
+  const letter = document.querySelectorAll(".letter");
+  const show = document.querySelectorAll(".show");
+  const overlay = document.querySelector("#overlay");
+  if (letter.length === show.length) {
+    overlay.className = "win";
+    overlay.firstElementChild.textContent = "You Won!";
+    overlay.style.display = "flex";
+  } else if (missed > 4) {
+    overlay.className = "lose";
+    overlay.firstElementChild.textContent = "Try Again!";
+    overlay.style.display = "flex";
+  }
+}
