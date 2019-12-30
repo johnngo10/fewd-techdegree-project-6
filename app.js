71d5292320ecb9e1434aa79bff5afb24c9964215
xuanngo10 <xuan.ngo10@gmail.com>
2019-12-30 15:06:28 -0800

    deleted unused variable

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const startButton = document.querySelector(".btn__reset");
-const scoreboard = document.querySelector("#scoreboard");
 const overlay = document.querySelector("#overlay");
 let missed = 0;
 
